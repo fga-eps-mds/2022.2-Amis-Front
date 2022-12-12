b994f4180071db9896c4a5c1a4e64106229f1bcc
André Lucas <andre.lucas.lemos@hotmail.com>
2022-12-12 13:34:06 -0300

    fix: Importação não usada quebrando deploy

--- src/shared/components/TablePagination/tablePagination.tsx
+++ src/shared/components/TablePagination/tablePagination.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
-import { Repository } from "../../../pages/alunas/alunas";
 
 interface TableProps {
   data: Object[];
