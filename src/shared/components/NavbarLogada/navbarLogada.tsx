abe427b70950d20385bf146573bf867bdd07f588
André Lucas <andre.lucas.lemos@hotmail.com>
2022-12-12 02:58:41 -0300

    (#52) Criando botão primário e ajustando layout da navbarLogada https://github.com/fga-eps-mds/2022.2-Amis-Doc/issues/52
    Co-authored-by: Caio Sulz <caiosulz14@gmail.com>

--- src/shared/components/NavbarLogada/navbarLogada.tsx
+++ src/shared/components/NavbarLogada/navbarLogada.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import styled from "styled-components";
 
 const DivNavbar = styled.div`
-  width: 100%;
+  width: 1054px;
   height: 100px;
   background: ${(props) => props.theme.colors.grey};
-  padding: 20px 60px;
+  padding: 20px 0px;
   display: flex;
   align-items: center;
+  justify-content: space-between;
+  margin: 0 auto;
 `;
 
 const Title = styled.h1`
@@ -21,13 +23,23 @@ const Title = styled.h1`
   align-items: center;
 `;
 
+const DivUser = styled.div`
+  display: inline-flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+const DivUserName = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
 const UserName = styled.h1`
   color: ${(props) => props.theme.colors.black};
   font-size: 15px;
   font-weight: 350;
   display: flex;
   align-items: center;
-  padding-left: 800px;
 `;
 
 const UserFunction = styled.h1`
@@ -36,16 +48,27 @@ const UserFunction = styled.h1`
   font-weight: 350;
   display: flex;
   align-items: center;
-  margin-top: 40px;
-  margin-left: -35px;
+  justify-content: flex-end;
+`;
+
+const UserImage = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 100%;
+  background-color: #525252;
 `;
 
 export function Navbarlog() {
   return (
     <DivNavbar>
       <Title> Alunas </Title>
-      <UserName>JOSÉ MARIA</UserName>
-      <UserFunction>Admin</UserFunction>
+      <DivUser>
+        <DivUserName>
+          <UserName>JOSÉ MARIA</UserName>
+          <UserFunction>Admin</UserFunction>
+        </DivUserName>
+        <UserImage />
+      </DivUser>
     </DivNavbar>
   );
 }
---
+++ src/shared/components/PrimaryButton/PrimaryButton.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styled from "styled-components";
+
+interface ButtonProps {
+  text: string;
+  handleClick?: () => void;
+}
+
+const Button = styled.button`
+  width: 150px;
+  height: 50px;
+  border-radius: 8px;
+  border: none;
+  cursor: pointer;
+  color: ${(props) => props.theme.colors.white};
+  background: ${(props) => props.theme.colors.primary};
+
+  &:hover {
+    background-color: #d2301e;
+  }
+`;
+
+export function PrimaryButton({ text, handleClick }: ButtonProps) {
+  return <Button onClick={handleClick}>{text}</Button>;
+}
+
+export default PrimaryButton;
