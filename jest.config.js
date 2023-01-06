64933c5508c78a2da6eace4bf66a2e4c5a9294c5
João Carvalho <jps12carvalho@gmail.com>
2023-01-06 16:34:48 -0300

    Configurando corretamente a cobertura de código

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,16 @@
 module.exports = {
+  coverageReporters: [
+    "json-summary", 
+    "text",
+    "lcov"
+  ],
+  collectCoverageFrom: [
+    "src/**/**/*.{ts,tsx}",
+    "src/**/**/**/*.{ts,tsx}",
+  ],
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/.jest/setup-tests.js'],
+  transform: {
+    ".*\\.[jt]sx?$": "babel-jest",
+  }
 }
--- src/tests/App.spec.tsx
+++ src/tests/App.spec.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 describe("Jest", () => {
   it("testing jest", () => {
     expect(1).toBe(1);
