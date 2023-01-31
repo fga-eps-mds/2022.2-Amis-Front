499a5d3cddea302b99f93d58cf3af6f5935d8091
paulohborba <paulohbr18@gmail.com>
2023-01-30 22:24:24 -0300

    (#116): Mudança do footer

--- src/shared/components/Footer/footer.tsx
+++ src/shared/components/Footer/footer.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import footer_image1 from "../../../assets/footer_image1.png";
+import footer_image2 from "../../../assets/footer_image2.png";
 
 const DivFooter = styled.div`
   width: 100%;
@@ -25,7 +26,13 @@ export function Footer() {
       <a href="https://www.instagram.com/amismulherescriativas/">
         <Image
           src={footer_image1}
-          style={{ width: "40px", marginBottom: "20px" }}
+          style={{ width: "40px", marginRight: "20x", marginBottom: "20px" }}
+        ></Image>
+      </a>
+      <a href="https://pt-br.facebook.com/InstitutoMulheresCriativas/">
+        <Image
+          src={footer_image2}
+          style={{ width: "20px", marginRight: "20px", marginBottom: "20px" }}
         ></Image>
       </a>
       <div>
